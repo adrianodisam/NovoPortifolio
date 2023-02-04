a5440c24775791767dd6890422d2c61ee11f8373
Adrianodisam <adrianodisam@gmail.com>
2023-02-04 14:37:03 -0300

    texto das tecnologias

--- src/componentes/Habilidades/Habilidades.js
+++ src/componentes/Habilidades/Habilidades.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import CaixaTecnologias from '../CaixaTecnologias/CaixaTecnologias';
+import Skills from '../Skills/Skills';
 import Styles from './Habilidades.module.css';
 
 const Habilidades = () => {
   return (
     <div id="Habilidades" className={Styles.Container}>
+      <Skills />
       <CaixaTecnologias />
     </div>
   );
---
+++ src/componentes/Skills/Skills.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Styles from './Skills.module.css';
+
+const Skills = () => {
+  return (
+    <div className={Styles.Container}>
+      <h3>Foco e skills</h3>
+      <p>
+        Me considero muito aplicado na resolução de problemas. Acho que
+        compartilhar soluções gera inspiração, quero ter a oportunidade e, no
+        momento favorável, ensinar esses passos para iniciantes, conforme eu fui
+        orientado e startado no mundo Dev.
+      </p>
+    </div>
+  );
+};
+
+export default Skills;
